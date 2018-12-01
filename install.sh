5e43e0dcfc22eb8b93dfec8b0faaa39f8fe55ba5
hmdeepin <jiftle@163.com>
2018-12-01 13:58:18 +0800

    做成安装脚本和卸载脚本，方便使用

---
+++ install.sh
@@ -0,0 +1,5 @@
+echo '--> start install .vimrc'
+cp .vimrc ~/
+echo 'copy .vimrc to user dir'
+cp -r .vim/ ~/
+echo '--> install successfully'
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+echo '--> start remove .vimrc'
+rm ~/.vimrc
+echo 'remove .vimrc from user dir'
+rm -rf ~/.vim/
+echo '--> uninstall successfully'
+
