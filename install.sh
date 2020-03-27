d2bc05f4830db37610f7fa531d7a832c9efddf42
devenv-vim <devenv-vim@docker.com>
2020-03-27 15:44:25 +0800

    处理NerdFont字体，状态栏图标乱码

--- install.sh
+++ install.sh
@@ -3,3 +3,16 @@ cp .vimrc ~/
 echo 'copy .vimrc to user dir'
 cp -r .vim/ ~/
 echo '--> install successfully'
+
+
+# 安装Nerd字体，解决状态栏图标显示乱码问题
+
+# 新建字体目录 
+sudo mkdir -p /usr/share/fonts/vim-nerd
+
+# 复制字体文件
+sudo cp ./fonts/* /usr/share/fonts/vim-nerd/
+
+# 刷新字体缓存
+sudo fc-cache -fv
+
